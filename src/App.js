5d2b1ab2faff86539ba489c00554978d5460e886
Keith Dumont <keithdumont@Keiths-MacBook-Pro.local>
2020-06-02 16:09:23 -0700

    added favicon/logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import Nav from "./Components/Nav";
 
 function App() {
   return (
-    <div className="App">
-      <h1>SHALOM!</h1>
+    <div className="App container">
+      <Nav />
     </div>
   );
 }
---
+++ src/Components/Nav.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <nav class="navbar navbar-light bg-light">
+      <a class="navbar-brand" href="#">
+        <img
+          src="/images/PrifinaVectorIcon.png"
+          width="30"
+          height="30"
+          class="d-inline-block align-bottom"
+          alt=""
+          loading="lazy"
+        />{" "}
+        Prifina
+      </a>
+    </nav>
+  );
+};
+
+export default Nav;
